dff3ad4c151017c0023cec894785142f494d127c
myg813p <gijungcpy@gmail.com>
2025-09-11 12:30:15 +0900

    test

--- test_playwright_headers.py
+++ test_playwright_headers.py
@@ -119,6 +119,7 @@ def test_requests_with_real_headers(num_requests=10):
                 
                 try:
                     response = session.post(GRAPHQL_URL, headers=headers, json=BODY, verify=False, timeout=2)
+                    time.sleep(.5)
                     
                     if response.status_code == 200:
                         success_count += 1
